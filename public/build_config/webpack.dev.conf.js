1178f8e5209f08c4608c59b79ae44d05b1985434
Susie Coleman <susie.coleman@guardian.co.uk>
2016-09-09 12:08:21 +0100

    WIP adding sass

--- public/build_config/webpack.dev.conf.js
+++ public/build_config/webpack.dev.conf.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: "./public/index.js",
   output: {
@@ -10,10 +13,15 @@ module.exports = {
         test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/
       },
       {
-        test: /\.jsx$/,
-        loaders: ['babel-loader'],
-        exclude: /node_modules/
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!sass-loader?sourceMap')
       }
     ]
-  }
+  },
+  sassLoader: {
+      includePaths: [path.resolve(__dirname, '../style')]
+  },
+  plugins: [
+      new ExtractTextPlugin('main.css')
+  ]
 };
