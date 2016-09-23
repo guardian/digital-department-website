749f8cb920bb13bb61dd776670f454b7b5683804
Susie Coleman <susie.coleman@guardian.co.uk>
2016-09-23 14:43:50 +0100

    Rename header to nav bar.

--- public/components/nav/nav.js
+++ public/components/nav/nav.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default class Header extends React.Component {
+export default class Nav extends React.Component {
   render() {
     return(
       <h1>
-        Header
+        Nav
       </h1>
     )
   }
